cf3fb8b98518e001a655f9488dd4052f489f101c
Emma Heang <kimheak868@gmail.com>
2025-04-26 16:28:05 +0700

    Update code

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(cors());
